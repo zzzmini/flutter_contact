9d613db71329781084628f676585e965541c1511
zzzmini <sm01079200887@gmail.com>
2025-04-10 12:14:38 +0900

    #2

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,14 @@ class Contact extends StatefulWidget {
 class _ContactState extends State<Contact> {
   var a = 1;
   var name = ['장원영', '안유진', '가을', '이서', '레이'];
+  var like = [0,0,0,0,0];
+
+  // 장원영 좋아요 수 증가(showDialog에서...)
+  addJangLike(){
+    setState(() {
+      like[0] = like[0] + 2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +46,25 @@ class _ContactState extends State<Contact> {
       body: Padding(
         padding: const EdgeInsets.only(top: 20),
         child: ListView.builder(
-          itemCount: 10,
+          itemCount: name.length,
           itemBuilder: (context, index){
             return ListTile(
               leading: Image.asset('assets/profile.JPG'),
-              title: Text('$index번째 홍길동'),
+              title: Text(name[index]),
+              trailing: TextButton(
+                onPressed: (){
+                  // 좋아요 증가시키기
+                  setState(() {
+                    like[index]++;
+                  });
+                },
+                child: Text('❤ ${like[index]}',
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 20
+                  ) ,
+                )
+              ),
             );
           }
         ),
@@ -57,10 +79,55 @@ class _ContactState extends State<Contact> {
             a++;
           });
           print('a = $a');
+
+          // Dialog 창 띄우기
+          showDialog(
+            context: context,
+            builder: (context)=>
+              DialogUI(
+                jangLike: like[0],
+                addJangLike : addJangLike,
+              )
+          );
         })
     );
   }
 }
+
+class DialogUI extends StatelessWidget{
+  const DialogUI({super.key, this.jangLike, this.addJangLike});
+  // 부모가 전달한 값을 받는 변수(리액트 props)
+  final jangLike;
+  final addJangLike;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton(
+              onPressed: (){
+                // 누르면 장원영 좋아요 2 증가
+                addJangLike();
+                Navigator.pop(context);
+              },
+              child: Text('장원영 좋아요 수? ${jangLike}')),
+            TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text('돌아가기'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class Bottom extends StatelessWidget{
   const Bottom({super.key});
